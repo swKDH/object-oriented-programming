a88ab322d83a2e5e254f097916bdb9ec99f7ac80
swKDH <39311129+swKDH@users.noreply.github.com>
2022-05-25 20:15:44 +0900

    0525 메모장 프로그램2
    메모장 프로그램을 구현 후 글꼴을 변경할 수 있는 버튼을 만들어보자
    
    using System;
    using System.Collections.Generic;
    using System.ComponentModel;
    using System.Data;
    using System.Drawing;
    using System.Linq;
    using System.Text;
    using System.Threading.Tasks;
    using System.Windows.Forms;
    using System.IO; //파일과 데이터 스트림에 대한 읽기 및 쓰기를 허용하는
                     //형식과 기본 파일 및 디렉터리 지원을 제공하는 형식을 포함
    
    namespace 메모장프로그램
    {
        public partial class Form1 : Form
        {
            public Form1()
            {
                InitializeComponent();
            }
    
            private void button1_Click(object sender, EventArgs e)
            {
                //파일오픈창 생성 및 설정
                OpenFileDialog ofd = new OpenFileDialog();
                ofd.Title = "파일 오픈창";
                ofd.FileName = "";
                ofd.Filter = "텍스트 파일 (*.txt) | *.txt";
    
                //파일 오픈창 로드
                DialogResult dr = ofd.ShowDialog();
    
                //OK버튼 클릭시
                if (dr == DialogResult.OK)
                {
    
                    //프로퍼티
                    //File명만 뽑는다
                    string fileNamel = ofd.SafeFileName.Substring(0, ofd.SafeFileName.Length - 4);
                    StreamReader sr = new StreamReader(ofd.FileName, Encoding.UTF8);
    
                    int position = ofd.FileName.LastIndexOf("\\");
    
                    string textFileName = ofd.FileName.Substring(position + 1);
    
                    this.Text = textFileName + " - Libo메모장";
    
                    //출력 로직
    
                    textBox1.Text = sr.ReadToEnd();
                    sr.Close();
    
    
                }
            }
    
            private void button2_Click(object sender, EventArgs e)
            {
                SaveFileDialog sfd = new SaveFileDialog();
    
                sfd.Filter = "텍스트 파일 (*.txt) | *.txt";
    
                if (sfd.ShowDialog() == DialogResult.OK)
    
                {
    
    
                    FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
    
                    StreamWriter sw = new StreamWriter(fs);
    
                    sw.WriteLine(textBox1.Text); // 파일 저장
                    sw.Close();
                }
            }
    
            private void button3_Click(object sender, EventArgs e)
            {
                fontDialog1.ShowDialog();
                textBox1.Font = fontDialog1.Font;
                textBox1.ForeColor = fontDialog1.Color;
            }
    
            private void fontDialog1_Apply(object sender, EventArgs e)
            {
    
            }
        }
    }

---
+++ notepad(수정본)/notepad(수정본)/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace notepad_수정본_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "열기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "저장";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(807, 412);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "글꼴";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ notepad(수정본)/notepad(수정본)/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //파일과 데이터 스트림에 대한 읽기 및 쓰기를 허용하는
+                 //형식과 기본 파일 및 디렉터리 지원을 제공하는 형식을 포함
+
+namespace notepad_수정본_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "파일 오픈창";
+            ofd.FileName = "";
+            ofd.Filter = "텍스트 파일 (*.txt) | *.txt";
+
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+
+                //프로퍼티
+                //File명만 뽑는다
+                string fileNamel = ofd.SafeFileName.Substring(0, ofd.SafeFileName.Length - 4);
+                StreamReader sr = new StreamReader(ofd.FileName, Encoding.UTF8);
+
+                int position = ofd.FileName.LastIndexOf("\\");
+
+                string textFileName = ofd.FileName.Substring(position + 1);
+
+                this.Text = textFileName + " - Libo메모장";
+
+                //출력 로직
+
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "텍스트 파일 (*.txt) | *.txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+
+            {
+
+
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+
+                StreamWriter sw = new StreamWriter(fs);
+
+                sw.WriteLine(textBox1.Text); // 파일 저장
+                sw.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            textBox1.Font = fontDialog1.Font;
+            textBox1.ForeColor = fontDialog1.Color;
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
